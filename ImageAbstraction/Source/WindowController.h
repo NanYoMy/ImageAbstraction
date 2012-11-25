0cfbbcbf03a20475d6770023c447a946b2c375ef
leonsenft <leonsenft@gmail.com>
2012-11-24 20:09:32 -0800

    Initial commit

---
+++ ImageAbstraction/Source/WindowController.h
@@ -0,0 +1,13 @@
+//
+//  WindowController.h
+//  ImageAbstraction
+//
+//  Created by Leon Senft on 2012-11-24.
+//  Copyright (c) 2012 Leon Senft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WindowController : NSWindowController
+
+@end
---
+++ ImageAbstraction/Source/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ImageAbstraction
+//
+//  Created by Leon Senft on 2012-11-24.
+//  Copyright (c) 2012 Leon Senft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
