6749ced8b8a9dbce6514352f3d6145d884cf6ada
leonsenft <leonsenft@gmail.com>
2012-11-25 09:32:22 -0800

    Initial Commit

---
+++ ImageAbstraction/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImageAbstraction
+//
+//  Created by Leon Senft on 2012-11-25.
+//  Copyright (c) 2012 Leon Senft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ImageAbstraction/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  ImageAbstraction
+//
+//  Created by Leon Senft on 2012-11-25.
+//  Copyright (c) 2012 Leon Senft. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
